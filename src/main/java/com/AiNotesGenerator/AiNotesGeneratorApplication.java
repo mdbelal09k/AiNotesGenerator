99cc9bcac9649f184c15acb2ad4b68d792310ccc
mygitbhubmd <mdbelalquraishi9546@gmail.com>
2026-06-17 15:48:08 +0530

    first edit

---
+++ src/main/java/com/AiNotesGenerator/AiNotesGeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.AiNotesGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiNotesGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiNotesGeneratorApplication.class, args);
+		System.out.println("java is the project ");
+	}
+
+}
---
+++ src/test/java/com/AiNotesGenerator/AiNotesGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AiNotesGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiNotesGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
